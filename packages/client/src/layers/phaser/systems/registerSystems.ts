b351963b78001dc344179a9f06200aafe18dea6e
empea-careercriminal <empeamtk@googlemail.com>
2023-05-19 21:48:10 +0200

    nuke project

--- packages/client/src/layers/phaser/systems/registerSystems.ts
+++
@@ -1,8 +0,0 @@
-import { PhaserLayer } from "../createPhaserLayer";
-import { createCamera } from "./createCamera";
-import { createMapSystem } from "./createMapSystem";
-
-export const registerSystems = (layer: PhaserLayer) => {
-  createCamera(layer);
-  createMapSystem(layer);
-};
